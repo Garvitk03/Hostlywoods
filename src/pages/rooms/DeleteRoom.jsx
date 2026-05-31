64d3a1b50de3b48fede9aebb133fa8e1f33d96bf
Garvitk03 <garvitkalra021@gmail.com>
2026-05-31 18:15:18 +0530

    Update DeleteRoom.jsx

--- src/pages/rooms/DeleteRoom.jsx
+++ src/pages/rooms/DeleteRoom.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import { successToast, errorToast } from '../../utils/toast';
+import { getAllRooms } from "../../services/roomService";
 
 const deleteRoom = async (id) => {
 
